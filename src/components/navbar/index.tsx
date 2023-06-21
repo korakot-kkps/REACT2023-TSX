bf1c8ad9fcc1703461819c800384d7b8d613006a
korakot-kkps <korakot.lonl@kkpfg.com>
2023-06-21 12:00:20 +0700

    save

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from "@mui/material";
+
+ function NavBar() {
+  return (
+    <Stack>
+      <Typography>Navbar</Typography>
+    </Stack>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,10 @@
+import { Stack, Typography } from "@mui/material";
+
+function Cart() {
+  return (
+    <Stack>
+      <Typography>Cart</Typography>
+    </Stack>
+  );
+}
+export default Cart;
